82a0e882a45a4ec8caaa5da7e7d28cd0157cfc9f
nurselaltin <nursel.altin@net-bt.com.tr>
2023-02-07 13:12:33 +0300

    init

---
+++ DepremOcrBot/DepremOcrBot/DepremOcrService.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+using RestSharp;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
+namespace DepremOcrBot
+{
+  internal class DepremOcrService
+  {
+    public static object JsonConvert { get; private set; }
+
+    public static void Upload_Text(Model m)
+    {
+
+
+      var client = new RestClient("https://mertcobanov-deprem-ocr-2.hf.space/run/upload-text");
+      RestRequest request = new RestRequest("POST");
+      var json = JsonSerializer.Serialize(m);
+      request.AddBody(json, "application/json");
+
+      var result = client.Execute(request);
+      var statusCode = result.StatusCode;
+      var mes = result.ErrorMessage;
+
+    }
+  }
+}
---
+++ DepremOcrBot/DepremOcrBot/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using DepremOcrBot;
+using RestSharp;
+using System.Text.Json;
+
+var model = new Model();
+model.data = new List<string>();
+model.data.Add("Bahçelievler Mahallesi Atatürk bulvarı no 334 daire 4  Adıyaman");
+DepremOcrBot.DepremOcrService.Upload_Text(model);
+
+
+Console.WriteLine("Hello, World!");
+
